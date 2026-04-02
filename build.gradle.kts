87892217a35f7290f2b8b4d530a8ca02f1992277
Maher <77703949+code-with-maher@users.noreply.github.com>
2026-04-02 20:48:45 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    id("com.android.application") version "8.0.2" apply false
-    id("com.android.library") version "8.0.2" apply false
-    kotlin("android") version "1.8.0" apply false
-    kotlin("kapt") version "1.8.0" apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("com.android.library") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
 
-    // مهم لفحص التحديثات
+    // فحص تحديثات المكتبات
     id("com.github.ben-manes.versions") version "0.51.0"
 }
