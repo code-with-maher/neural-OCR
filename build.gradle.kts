d84c46133209d60ea6f17902102d7cd326a583f5
Maher <77703949+code-with-maher@users.noreply.github.com>
2026-04-02 20:37:39 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,7 @@ plugins {
     id("com.android.library") version "8.0.2" apply false
     kotlin("android") version "1.8.0" apply false
     kotlin("kapt") version "1.8.0" apply false
-}
 
-android {
-    compileSdk = 34
-}
-
-dependencies {
-    implementation("androidx.compose.ui:ui:1.5.0")
-    implementation("androidx.compose.material3:material3:1.5.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
+    // مهم لفحص التحديثات
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
