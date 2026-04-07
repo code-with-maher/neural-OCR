2b341e0c2ee2fa8aca2f00dfd3bfeb08220f8c8c
Maher <77703949+code-with-maher@users.noreply.github.com>
2026-04-07 09:49:44 +0300

    Create app/src/main/java/com/a/labs/worker/PlaybackService.kt

---
+++ app/src/main/java/com/a/labs/worker/PlaybackService.kt
@@ -0,0 +1,41 @@
+package com.a.labs.worker
+
+import android.content.Intent
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+
+class PlaybackService : MediaSessionService() {
+
+    private var mediaSession: MediaSession? = null
+    private lateinit var player: Player
+
+    override fun onCreate() {
+        super.onCreate()
+        player = ExoPlayer.Builder(this).build()
+        mediaSession = MediaSession.Builder(this, player).build()
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+        return mediaSession
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        val player = mediaSession?.player
+        if (player != null) {
+            if (!player.playWhenReady || player.mediaItemCount == 0) {
+                stopSelf()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        mediaSession?.run {
+            player.release()
+            release()
+            mediaSession = null
+        }
+        super.onDestroy()
+    }
+}
