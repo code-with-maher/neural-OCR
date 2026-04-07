99d8c03a7fc296346cb16300869c85eac0705d83
Maher <77703949+code-with-maher@users.noreply.github.com>
2026-04-07 10:46:21 +0300

    Create app/src/main/java/com/a/labs/ui/navigation/NavGraph.kt

---
+++ app/src/main/java/com/a/labs/ui/navigation/NavGraph.kt
@@ -0,0 +1,45 @@
+package com.a.labs.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.a.labs.ui.ViewModelFactory
+import com.a.labs.ui.library.LibraryScreen
+import com.a.labs.ui.library.LibraryViewModel
+import com.a.labs.ui.reader.ReaderScreen
+import com.a.labs.ui.reader.ReaderViewModel
+import com.a.labs.ui.settings.SettingsScreen
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+    factory: ViewModelFactory,
+    readerFactory: ViewModelFactory
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "library"
+    ) {
+        composable("library") {
+            val viewModel: LibraryViewModel = viewModel(factory = factory)
+            LibraryScreen(navController, viewModel)
+        }
+        
+        composable("settings") {
+            SettingsScreen(navController)
+        }
+        
+        composable(
+            route = "reader/{bookId}",
+            arguments = listOf(navArgument("bookId") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val bookId = backStackEntry.arguments?.getString("bookId") ?: ""
+            val viewModel: ReaderViewModel = viewModel(factory = readerFactory)
+            ReaderScreen(navController, viewModel, bookId)
+        }
+    }
+}
