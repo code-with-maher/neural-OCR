dfefdba366ce12f930b16416d4cfd50344a28811
Maher <77703949+code-with-maher@users.noreply.github.com>
2026-04-15 12:28:21 +0300

    Create app/src/main/java/com/a/labs/GeminiOCRApp.kt

---
+++ app/src/main/java/com/a/labs/GeminiOCRApp.kt
@@ -0,0 +1,14 @@
+package com.a.labs
+
+import android.app.Application
+import com.a.labs.core.crash.GlobalCrashHandler
+
+class GeminiOCRApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        
+        // إعداد الفخ: تسجيل معالج الأخطاء فور تشغيل التطبيق
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler(GlobalCrashHandler(this, defaultHandler))
+    }
+}
