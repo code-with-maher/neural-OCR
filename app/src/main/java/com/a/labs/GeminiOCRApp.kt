95aefa8aedd84777f46c61189c1ef87d9621e4ea
Maher <77703949+code-with-maher@users.noreply.github.com>
2026-07-19 11:43:33 +0300

    Update GeminiOCRApp.kt

--- app/src/main/java/com/a/labs/GeminiOCRApp.kt
+++ app/src/main/java/com/a/labs/GeminiOCRApp.kt
@@ -6,8 +6,7 @@ import com.a.labs.core.crash.GlobalCrashHandler
 class GeminiOCRApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        
-        // إعداد الفخ: تسجيل معالج الأخطاء فور تشغيل التطبيق
+
         val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
         Thread.setDefaultUncaughtExceptionHandler(GlobalCrashHandler(this, defaultHandler))
     }
